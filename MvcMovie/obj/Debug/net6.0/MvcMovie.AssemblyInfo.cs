65f79f5eccd84275bc2642b74ba6a37ef8eb4936
Colin Beeby <colin.beeby@hee.nhs.uk>
2024-01-02 16:11:26 +0000

    another go at getting actions to run migrations

--- MvcMovie/obj/Debug/net6.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net6.0/MvcMovie.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b1766323220225a8f5bb04ad4f709f4a96efbc0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
