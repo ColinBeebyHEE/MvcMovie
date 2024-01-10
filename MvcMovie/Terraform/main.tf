f463ce0cf85d0484695a0871a2ebeb3495eb5542
ColinBeebyHEE <125499287+ColinBeebyHEE@users.noreply.github.com>
2024-01-10 10:04:09 +0000

    Update main.tf

--- MvcMovie/Terraform/main.tf
+++ MvcMovie/Terraform/main.tf
@@ -2,3 +2,11 @@ resource "azurerm_resource_group" "resourcegroups" {
     name        = var.ResourceGroup
     location    = var.Location
 }
+
+resource "azurerm_service_plan" "example" {
+  name                = "colins-mvc-movie-app-service-plan"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  sku_name			  = "B1"
+  os_type			  = "Linux"
+}
