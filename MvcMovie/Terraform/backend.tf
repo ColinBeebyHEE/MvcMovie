eca3aadb3e8226bd0efd4d7a0b2d68964c49fb67
ColinBeebyHEE <125499287+ColinBeebyHEE@users.noreply.github.com>
2024-01-10 08:17:39 +0000

    Update backend.tf

--- MvcMovie/Terraform/backend.tf
+++ MvcMovie/Terraform/backend.tf
@@ -7,7 +7,7 @@ terraform {
   }
   backend "azurerm" {
     resource_group_name     = "NE-ELFH-HUB-STOR-RG"
-    storage_account_name    = "elfhhubdevstore"
+    storage_account_name    = "elfhhubdevstor"
     container_name          = "tfstate"
     key                     = "colin.terraform.tfstate"
   }
