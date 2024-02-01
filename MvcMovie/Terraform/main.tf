6c3a7ed9b5d6af922e2c2e8aee336dacb8efab15
Colin Beeby <Colin.Beeby@hee.nhs.uk>
2024-02-01 15:40:03 +0000

    dd

--- MvcMovie/Terraform/main.tf
+++ MvcMovie/Terraform/main.tf
@@ -33,16 +33,16 @@ resource "azurerm_mssql_server" "MvcMovieMssqlServer" {
   resource_group_name		    = azurerm_resource_group.MvcMovieResourceGroup.name
 }
 
-resource "github_actions_variable" "MvcMovieConnectionString" {
-  repository     = "MvcMovie"
-  variable_name  = "MOVIE_DB_CONNECTION"
-  value = "Server=tcp:${azurerm_mssql_server.MvcMovieMssqlServer.name},1433;Initial Catalog=${azurerm_mssql_database.MvcMovieMssqlDatabase.name};Persist Security Info=False;User ID=exampleadmin;Password=Ex@mpleP@ssword123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
-}
-
 resource "azurerm_mssql_database" "MvcMovieMssqlDatabase" {
   name                = "colins-mvc-movie-mssql-database"
   collation           = "SQL_Latin1_General_CP1_CI_AS"
   server_id			  = azurerm_mssql_server.MvcMovieMssqlServer.id
+}
+
+resource "github_actions_variable" "MvcMovieConnectionString" {
+  repository     = "MvcMovie"
+  variable_name  = "MOVIE_DB_CONNECTION"
+  value = "Server=tcp:${azurerm_mssql_server.MvcMovieMssqlServer.name},1433;Initial Catalog=${azurerm_mssql_database.MvcMovieMssqlDatabase.name};Persist Security Info=False;User ID=exampleadmin;Password=Ex@mpleP@ssword123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
 }
 
 resource "azurerm_mssql_firewall_rule" "appServiceIP" {
